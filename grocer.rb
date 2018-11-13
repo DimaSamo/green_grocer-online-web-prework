458da55847c780282204e28666b1db3979f924a3
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-13 04:08:32 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -50,7 +50,7 @@ end
 
 def apply_clearance(cart)
   cart.each do |item, data|
-    data[:price] *= 0.8 if data[:clearance]
+    data[:price] *= (0.8 if data[:clearance]).round(1)
   end
   cart
 end
